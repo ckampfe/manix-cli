a21e095531e17121ad673bfa7e457b0e317c2aad
Clark Kampfe <clark.kampfe@gmail.com>
2021-08-06 15:50:42 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let f = std::fs::File::open("/home/clark/code/personal/bex/Root_Vase.stl.torrent")?;
+    let t = std::thread::spawn(|| {
+        let f = std::fs::File::open("/Users/clark/code/manix/Root_Vase.stl.torrent").unwrap();
 
-    let options = manix::Options::default();
-    let mut manix = manix::blocking_client(options);
+        let options = manix::Options::default();
+        let mut manix = manix::blocking_client(options);
 
-    let out = Box::new(std::io::Cursor::new(vec![]));
+        let out = Box::new(std::io::Cursor::new(vec![]));
 
-    let torrent = manix.add_torrent(f, out).unwrap();
+        let torrent = manix.add_torrent(f, out).unwrap();
 
-    println!("here");
-    println!("announce_url: {}", torrent.get_announce_url());
-    println!("info_hash: {}", torrent.get_info_hash_human());
-    println!("peer_id: {}", torrent.get_peer_id().human_readable());
+        println!("here");
+        println!("announce_url: {}", torrent.get_announce_url());
+        println!("info_hash: {}", torrent.get_info_hash_human());
+        println!("peer_id: {}", torrent.get_peer_id().human_readable());
 
-    let info_hash = torrent.get_info_hash_human();
-    manix.start_torrent(&info_hash)?;
+        let info_hash = torrent.get_info_hash_human();
+        println!("PRESTART-----------------");
+        manix.start_torrent(&info_hash).unwrap();
+        println!("STARTED---------------------------");
+        std::thread::sleep(std::time::Duration::from_secs(10));
+        println!("AFTER SLEEP------------------------");
+        manix.pause_torrent(&info_hash).unwrap();
+        std::thread::sleep(std::time::Duration::MAX)
+    });
+
+    t.join().unwrap();
 
     Ok(())
 }
